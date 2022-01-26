763164e022b126111b43d8c19517fcbac0e63d7b
Hadi <bcsf18a508@pucit.edu.pk>
2022-01-26 16:06:38 +0500

    Added Person database handler

---
+++ app/src/main/java/com/example/todolist/PersonDB.java
@@ -0,0 +1,25 @@
+package com.example.todolist;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class PersonDB extends SQLiteOpenHelper {
+
+    public PersonDB(Context context,String table){
+        super(context,table,null,1 );
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE person.db ( ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                " username TEXT NOT NULL, password TEXT NOT NULL)";
+
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
