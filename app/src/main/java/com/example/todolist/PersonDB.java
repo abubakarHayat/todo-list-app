68ebc40d2f7fee68d495d4fb310e0c6ae3bac2aa
umar <bcsf18a502@pucit.edu.pk>
2022-01-26 18:38:55 +0500

    Added PersonDB class functionality

--- app/src/main/java/com/example/todolist/PersonDB.java
+++ app/src/main/java/com/example/todolist/PersonDB.java
@@ -2,8 +2,12 @@ package com.example.todolist;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PersonDB extends SQLiteOpenHelper {
 
@@ -34,5 +38,42 @@ public class PersonDB extends SQLiteOpenHelper {
         long result = db.insert("person.db", null,cv);
         db.close();
         return result;
+    }
+
+    public boolean deleteOne(Person p){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String deleteRecord = "DELETE FROM person.db WHERE id =" + p.getId();
+        Cursor cursor = db.rawQuery(deleteRecord,null);
+        if(cursor.moveToFirst()){
+            cursor.close();
+            db.close();
+            return true;
+        }
+        cursor.close();
+        db.close();
+        return false;
+
+    }
+    public List<Person> getAll(){
+        List<Person> retList = new ArrayList<>();
+        String readData = "SELECT * FROM person.db";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(readData,null);
+        if(cursor.moveToFirst()){
+            do{
+                int id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                String pass = cursor.getString(2);
+                Person p = new Person(id,name,pass);
+                retList.add(p);
+
+            }while(cursor.moveToNext());
+        }else{
+            //empty list
+        }
+        cursor.close();
+        db.close();
+        return retList;
+
     }
 }
